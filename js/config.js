be182919b6ce2fb5e5ec8f0092f661967b1add93
hamzath3433 <f8wwkw6v7f@privaterelay.appleid.com>
2026-09-24 10:40:46 +0500

    Rename brand to Foaru (foaru.com)

--- js/config.js
+++ js/config.js
@@ -1,9 +1,6 @@
-/* Change this before going live. Admin can also override it from the desk. */
 window.VARU_CONFIG = {
-  adminEmail: "admin@varu.mv",
-  adminName: "Varu Master Admin",
-  brandEmail: "hello@varu.mv",
-  phone: "+960 799 2400",
-  whatsapp: "9607992400",
+  adminEmail: "admin@foaru.com",
+  adminName: "Foaru Master Admin",
+  brandEmail: "hello@foaru.com",
   formEndpoint: "https://formsubmit.co/ajax/"
 };
